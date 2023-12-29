b89b897c93664d88f833e34c60b568cfd1ad5784
corfyi <corfyi@163.com>
2023-12-29 12:03:10 +0800

    generate file val_seqmap.txt

--- eval_mot17.py
+++ eval_mot17.py
@@ -8,4 +8,15 @@ exp_name = "val"
 
 seqmap = os.path.join(out_path,exp_name, "val_seqmap.txt")
 
+# 生成val_seqmap.txt文件
+with open(seqmap,"w") as f:
+    f.write("name\n")
+    f.write("MOT17-02-SDP\n")
+    f.write("MOT17-04-SDP\n")
+    f.write("MOT17-05-SDP\n")
+    f.write("MOT17-09-SDP\n")
+    f.write("MOT17-10-SDP\n")
+    f.write("MOT17-11-SDP\n")
+    f.write("MOT17-13-SDP\n")
+
 HOTA,IDF1,MOTA,AssA = eval(dataset_path,out_path, seqmap, exp_name,1,False)
